4bf9e4082365f21194a4f423612a62f11c19ef61
Andy South <southandy@gmail.com>
2017-02-21 14:17:10 +0000

    version 0.1.0

---
+++ R/rnaturalearthdata-package.r
@@ -0,0 +1,19 @@
+#' rnaturalearthdata : World Vector Map Data from Natural Earth Used in 'rnaturalearth'
+#'
+#' holds data used by rnaturalearth package from \href{http://www.naturalearthdata.com/}{Natural Earth}.
+#'
+#' @name rnaturalearthdata
+#' @docType package
+# @seealso \code{\link[rnaturalearth]{ne_countries}} \code{\link[rnaturalearth]{ne_states}} \code{\link[rnaturalearth]{ne_download}}
+#' @import sp
+NULL
+
+# Hide variables from R CMD check
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(c("countries110", "countries50",
+                           "map_units110", "map_units50",
+                           "sovereignty110", "sovereignty50",
+                           "coastline110", "coastline50",
+                           "states50"
+                           ))
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+[![Travis-CI Build Status](https://travis-ci.org/ropenscilabs/rnaturalearthdata.svg?branch=master)](https://travis-ci.org/ropenscilabs/rnaturalearthdata)
+
+# rnaturalearthdata
+
+An R package to store data for the [rnaturalearth](https://github.com/ropenscilabs/rnaturalearth) package.
+
+
+[![ropensci\_footer](http://ropensci.org/public_images/github_footer.png)](http://ropensci.org)
